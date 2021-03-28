19b4fa6569cccbf674b39fccf47080341361c949
Momi786 <ramaljan786@gmail.com>
2021-03-29 00:26:26 +0500

    new pages

---
+++ page-not/index.php
@@ -0,0 +1,18 @@
+<?php
+    include '../include/header2.php';
+?>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 page-not">
+            <h1>404</h1>
+<p>Oops! Something is wrong.</p>
+<a class="button" href="#"><i class="icon-home"></i> Go back to Home Page.</a>
+            </div>
+        </div>
+    </div>
+
+<?php
+    include '../include/footer2.php';
+?>
--- shoping-cart/index.php
+++ shoping-cart/index.php
@@ -2,54 +2,63 @@
     include '../include/header.php';
 ?>
     <section>
-    <div>
+    <div class="cart-bg">
         <div class="container">
-            <h1>Shoping Cart</h1>
+          <div class="row">
+            <div class="col-md-12 cart-heading">
+                <h1>Shoping Cart</h1>
+            </div>
+          </div>
         </div>
 
     </div>
 
     </section>
 
-    <div class="container">
+    <div class="container mt-6">
 
     
 
-<section class="mt-5">
+<section class="">
 <div class="shopping-cart">
 
 <div class="column-labels">
   <label class="product-image">Image</label>
-  <label class="product-details">Product</label>
-  <label class="product-price">Price</label>
+  <label class="product-details">Product</label> 
   <label class="product-quantity">Quantity</label>
-  <label class="product-removal">Remove</label>
+  <label class="product-price">Price</label>
   <label class="product-line-price">Total</label>
 </div>
 
 <div class="product">
   <div class="product-image">
-    <img src="../img/book.jpg" class="img-fluid w-100 h-100 mr-3">
+    <img src="../img/book.jpg" class="img-fluid mr-3">
   </div>
   <div class="product-details">
     <div class="product-title">book</div>
     <p class="product-description">The best Book</p>
   </div>
-  <div class="product-price">12.99</div>
   <div class="product-quantity">
     <input type="number" class="border-0" value="2" min="1">
   </div>
-  <div class="product-removal">
-    <button class="remove-product">
-      Remove
-    </button>
-  </div>
+  <div class="product-price">12.99</div>
   <div class="product-line-price">25.98</div>
 </div>
 
 
 
-<div class="totals">
+
+    <div class="row">
+      <div class="col-md-6">
+        <div class="d-flex">
+          <div class="form-input mr-3">
+            <input type="text" class="form-control" placeholder="Enter Code Here">
+          </div>
+          <button class="btn btn-coupan">APPLY COUPAN</button>
+        </div>
+      </div>
+      <div class="col-md-6">
+      <div class="totals">
   <div class="totals-item">
     <label>Subtotal</label>
     <div class="totals-value" id="cart-subtotal">71.97</div>
@@ -67,12 +76,16 @@
     <div class="totals-value" id="cart-total">90.57</div>
   </div>
 </div>
+      </div>
+    </div>
     
-    <button class="checkout">Checkout</button>
 
 </div>
 
-
+<div class="d-flex justify-content-end mt-3">
+    <button class="btn btn-checkout mr-3">Continue Shoping</button>
+    <button class="btn btn-checkout">Process to checkout</button>
+    </div>
 </section>
 
 
@@ -80,6 +93,3 @@
     include '../include/footer.php';
 ?>
 
-<script>
-   
-</script>
